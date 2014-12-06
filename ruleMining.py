a37004b591a7e92bb28d76116ea7f000ed1ccb8c
Jeremy <ledonne.5@osu.edu>
2014-12-06 09:14:03 -0500

    Finished implementing the classification

--- ruleMining.py
+++ ruleMining.py
@@ -1,6 +1,8 @@
 import os
+import glob
 import random
 import math
+import time
 from optparse import OptionParser
 
 class ruleMining:
@@ -48,19 +50,63 @@ class ruleMining:
             ouf.write(cluster[i])
             ouf.write("\n")
         ouf.close()
-        outfileNames = []
         outfile = "rules" + str(num) + ".txt"
         #cluster is a list of strings
-        command = "./apriori -k, -m2 -tr -o -s" + str(sup) + " -c" + str(conf) + " infile.txt " + outfile 
+        command = "apriori.exe -k, -m2 -tr -o -s" + str(sup) + " -c" + str(conf) + " infile.txt " + outfile 
         print ("Running command: ", command)
+        print ("Cluster was size:" + str(len(cluster)))
         os.system(command)
-        #return list of rule file names
-        outfileNames.extend(outfile)       
-	return outfileNames
-             
-    def generateClassifiers(self, rules):
-        pass
-
+        #return list of rule file names       
+        return outfile
+     
+    #Creates the rule classifiers by pruning the rules and ordering them by confidence followed by support    
+    def generateClassifiers(self, ruleFiles, classes):
+        ruleClassifiers = []
+        for file in ruleFiles:
+            inf = open(file, "r")
+            linedump = inf.readlines()
+            inf.close()
+            if len(linedump) < 2: 
+                ruleClassifiers.append(["defaultLabel0"])
+                continue
+            if len(linedump[-1]) < 3: del linedump[-1] #Remove empty line
+           
+            #Prune unwanted rules
+            classFrequencies = {}
+            goodRules = []
+            for aprioriRule in linedump:
+                (isValid, rule) = self.isValidRule(aprioriRule, classes)
+                if isValid: 
+                    goodRules.append(rule)
+                    if rule[1] in classFrequencies: classFrequencies[rule[1]] += 1
+                    else: classFrequencies[rule[1]] = 1
+            
+            orderedRules = sorted(goodRules, key = lambda x: (x[3], x[2]), reverse=True)
+            if len(classFrequencies) == 0: 
+                dominantClass = "defaultLabel0"
+            else:
+                dominantClass = max(classFrequencies, key=classFrequencies.get)
+            orderedRules.append(dominantClass)
+            ruleClassifiers.append(orderedRules)
+   
+        return ruleClassifiers
+              
+    #Checks to see if the rule generated from the Apriori rule is valid and prunes          
+    def isValidRule(self, rule, classes):
+        arrow = rule.find("<")
+        paren = rule.find("(", arrow)
+        classification = rule[:arrow].replace(" ","")
+        words = rule[arrow+2:paren].replace(" ","").split(",")
+        support = float(rule[paren+1:rule.find(",", paren)])
+        confidence = float(rule[rule.rfind(",")+1:rule.rfind(")")].strip(" "))
+        
+        if classification == '' or words[0] == '': return (False, [])
+        if classification not in classes: return (False, []) #Remove if the consequent is a word and not a class
+        for word in words: #If any classes are in the antecedent, remove them
+            if word in classes: return (False, [])
+            
+        return (True, [words, classification, support, confidence])
+              
 #Call kmeans progrmam to cluster
 def callKMeans(trainingClusteringList, numClusters, distance):
    # return [[],[]]
@@ -78,7 +124,6 @@ def callKMeans(trainingClusteringList, numClusters, distance):
         sCents = line.strip("\n").split(",")
         cents = [float(a) for a in sCents]
         centroids.append(cents)
-    #return the centroids
     return centroids
     #return cluster centroids in the form: list of lists
     #centroids = [[centroid1], ...., [centroidN]]
@@ -102,9 +147,6 @@ def createWekaFile(clusterList):
 
     outf.close()
 
-   # for attribute in attributes:
-   #         header = header + "@ATTRIBUTE " + attribute + " NUMERIC\n"
-
 #Set up command line options
 def parseOptions():
     parser = OptionParser()
@@ -112,10 +154,12 @@ def parseOptions():
     conf = 60
     numC = 1
     dist = 1
+    sampling = 200000
     parser.add_option("-s", "--support", dest="sup", action="store")
     parser.add_option("-c", "--confidence", dest="conf", action="store")
     parser.add_option("-k", "--numOfClusters", dest="numClusters", action="store")
     parser.add_option("-d", "--distance", dest="distance", action="store")
+    parser.add_option("-a", "--sampling", dest="sample", action="store")
     (options, args) = parser.parse_args()
     if options.sup:
         sup = options.sup
@@ -125,37 +169,51 @@ def parseOptions():
         numC = options.numClusters
     if options.distance:
         dist = options.distance
-    return (int(sup), int(conf), int(numC), int(dist))        
+    if options.sample:
+        sampling = options.sample
+    return (int(sup), int(conf), int(numC), int(dist), int(sampling))        
  
-#Partitions the data into 10 different sets used for 10-fold cross validation
-def partitionData():
+#Partitions the data into 5 different sets used for 10-fold cross validation
+def partitionData(sampling):
     inf = open("FV3.txt", "r")
     linedump = inf.readlines()
     inf.close()
     
-    partitions = [[],[],[],[],[],[],[],[],[],[]]
+    partitions = [[],[],[],[],[]]
+    allClasses = set([]) #Keeps track of all the classes we have (topics and places)
     index = 0
     size = len(linedump)
-
+    sample = 0
+    
     #Randomly sample the data and place into partitions
     for num in range(size):
         randomIndex = random.randrange(0,len(linedump))
         line = linedump[randomIndex].replace("'","").rstrip("\n")
         del linedump[randomIndex]
-        if hasNoClass(line): #skip if there are no topics or places
+        (hasNoClass, classes) = checkForClasses(line)
+        if hasNoClass: #skip if there are no topics or places
             continue
+        else:
+            for word in classes: allClasses.add(word)
+            sample += 1
         partitions[index].append(line)
-        index = (index+1)%10
-    return partitions
+        index = (index+1)%5
+        if sample == sampling: break
+    return (partitions, allClasses)
 
-def hasNoClass(line):
+def checkForClasses(line):
     topics_pos = line.rfind("<")
-    topics = line[topics_pos+1:line.rfind(">")].split(",")
-    places = line[line[:topics_pos].rfind("<")+1:topics_pos-1].split(",")
+    topics = line[topics_pos+1:line.rfind(">")].replace(" ","").split(",")
+    places = line[line[:topics_pos].rfind("<")+1:topics_pos-1].replace(" ","").split(",")
     if len(topics) == 1 and topics[0] == '' and len(places) == 1 and places[0] == '':
-        return True
+        return (True, [])
     else:
-        return False
+        classes = []
+        for c in topics:
+            if c != '': classes.append(c)
+        for c in places:
+            if c != '': classes.append(c)
+        return (False, classes)
 
 #Generates a list of the data to be clustered.  From the trainingSet, this function
 #finds the corresponding feature fector in FV4.txt and appends it to the list.
@@ -202,28 +260,15 @@ def generateRulePartitions(centroids, trainingSet, distanceMetric):
     
     clusters = []
     
-#    inf2 = open("centroids.txt","r")
-#    lines = inf2.readlines()
-#    inf2.close()
-#    centroids = [[float(a) for a in b.strip("\n").split(",")] for b in lines]
-    
     for num in range(len(centroids)):
         clusters.append([])
     
     for vector in trainingSet:
         position = int(vector[1:vector.find(",")])
-        line = linedump[position]
-        line = line[1:line.find(">")].replace(" ","").split(",")
-        dataVector = [float(a) for a in line[1:]]
-        minDistance = -1
-        minDistanceCluster = 0
-        for num, centroid in enumerate(centroids):
-            distance = distanceBetweenVectors(centroid, dataVector, distanceMetric)
-            if distance < minDistance or minDistance == -1:
-                minDistance = distance
-                minDistanceCluster = num
+        minDistanceCluster = findClosestCentroid(linedump[position], centroids, distanceMetric)
         clusters[minDistanceCluster].append(createAprioriDataVector(vector))        
-        
+    del linedump
+     
     return clusters    
  
 #Computes the distance between vector1 and vector2
@@ -242,47 +287,138 @@ def distanceBetweenVectors(vector1, vector2, distanceMetric):
 #that can be used by the Apriori software
 def createAprioriDataVector(vector):
     class_pos = vector.find("<", 2)
-    words = vector[vector.find(",")+1:class_pos-1].replace(" ","") + ","
-    places = vector[class_pos+1:vector.find(">",class_pos)].replace(" ","") + ","
+    words = vector[vector.find(",")+1:class_pos-1].replace(" ","") + "," #Extract words
+    places = vector[class_pos+1:vector.find(">",class_pos)].replace(" ","") + "," #Extract places
     if places == ",": places = ""
-    topics = vector[vector.rfind("<")+1:vector.rfind(">")].replace(" ","")
+    topics = vector[vector.rfind("<")+1:vector.rfind(">")].replace(" ","") #Extract topics
     if topics == "": places = places[:-1]
     s = words + places + topics
     return s
  
+#Finds the closest cluster centroid the data belongs to 
+def findClosestCentroid(line, clusterCentroids, distanceMetric):
+    line = line[1:line.find(">")].replace(" ","").split(",")
+    dataVector = [float(a) for a in line[1:]]
+    minDistance = -1
+    minDistanceCluster = 0
+    for num, centroid in enumerate(clusterCentroids):
+        distance = distanceBetweenVectors(centroid, dataVector, distanceMetric)
+        if distance < minDistance or minDistance == -1:
+            minDistance = distance
+            minDistanceCluster = num
+    return minDistanceCluster        
+ 
+#Looks through the generated rules and assigns a class label to the point
+def classify(ruleSet, vector):
+    if len(ruleSet) == 1: return ruleSet[0]
+    vectorWords = vector[1:vector.find(">")].replace(" ","").split(",")
+    vectorWords = vectorWords[1:]
+    vectorWordSet = set(vectorWords)
+    for rule in ruleSet[:-1]:
+        ruleSetWords = set(rule[0])
+        for word in ruleSetWords:
+            if word not in vectorWordSet: break
+        else: #Executes when no break is encountered (means all the words in the rule set were found in the test feature vector)
+            return rule[1]
+    return ruleSet[-1]
+
+#Returns 1 if the classLabel matches the topics or places of the vector and returns 0 otherwise    
+def isCorrectClassification(classLabel, vector):
+    class_pos = vector.find("<", 2)
+    places = vector[class_pos+1:vector.find(">",class_pos)].replace(" ","").split(",")
+    topics = vector[vector.rfind("<")+1:vector.rfind(">")].replace(" ","").split(",")
+    if classLabel in places or classLabel in topics: return 1
+    else: return 0
+    
+#Prints results to a file    
+def printResults(fold_accuracies, totalTime, aprioriTime, clusteringTime, clusterNum, distanceMetric, support, confidence):
+    outf = open("results.txt", "a")
+    s = ""
+    if clusterNum == 1:
+        s = "Clustering: None"
+    else:
+        s = "Clustering: " + str(clusterNum) + " clusters,"
+        if distanceMetric == 1:
+            s = s + " Manhattan distance"
+        else:
+            s = s + " Euclidean distance"
+    s = s + "\nSupport: " + str(support) + "%    Confidence: " + str(confidence) + "%\n"
+    s = s + "Accuracy: " + str(sum(fold_accuracies)/float(len(fold_accuracies))) + "\n"
+    s = s + "Total run time: " + str(totalTime) + "    Clustering time: " + str(clusteringTime) + "    Apriori Rule Creation Time: " + str(aprioriTime) + "\n\n\n"
+    outf.write(s)
+    outf.close()
+
+#Cleans up the extra clutter in the directory    
+def cleanDirectory():
+    os.remove("infile.txt")
+    if os.path.exists("centroids.txt"): os.remove("centroids.txt")
+    rulesFiles = glob.glob("rules*")
+    for file in rulesFiles:
+        os.remove(file)
+    
 def main():
-    (support, confidence, clusterNum, distanceMetric) = parseOptions()
+    (support, confidence, clusterNum, distanceMetric, sampling) = parseOptions()
+    startTime = time.time()
+    clusteringTime = 0
+    aprioriTime = 0
     rm = ruleMining()
-    partitions = partitionData() 
+    (partitions, classes) = partitionData(sampling)  
+    fold_accuracies = []
     for num, partition in enumerate(partitions):
         (trainingSet, testSet) = generateTrainingTestSets(partitions, num) 
+        correct = 0
         if clusterNum > 1:  #Perform Clustering
             trainingClusteringList = generateClusteringSet(trainingSet)
+            cStartTime = time.time()
             clusterCentroids = callKMeans(trainingClusteringList, clusterNum, distanceMetric) 
-            clusters = generateRulePartitions(clusterCentroids, trainingSet, distanceMetric) #TODO: jeremy
+            cEndTime = time.time()
+            clusteringTime += cEndTime - cStartTime
+            clusters = generateRulePartitions(clusterCentroids, trainingSet, distanceMetric)
             rules = []
-            i=1
-            for cluster in clusters:
-                rules.append(rm.callApriori(cluster, support, confidence, i)) #TODO: manjari
-                i = i+1
-            classifiers = rm.generateClassifiers(rules)     #TODO:jeremy
-            #TODO: classify elements in testSet and record analytics
+            for cNum, cluster in enumerate(clusters):
+                aStartTime = time.time()
+                rules.append(rm.callApriori(cluster, support, confidence, cNum+1))    
+                aEndTime = time.time()
+                aprioriTime += aEndTime - aStartTime
+            classifiers = rm.generateClassifiers(rules, classes)   
+            
+            #Test classifiers on the test set
+            inf = open("FV4.txt", "r")
+            linedump = inf.readlines()
+            inf.close()
+            print ("Classifying test set using generated rules...")
+            for vector in testSet:
+                position = int(vector[1:vector.find(",")])
+                closestCentroid = findClosestCentroid(linedump[position], clusterCentroids, distanceMetric)
+                classLabel = classify(classifiers[closestCentroid], vector)
+                correct = correct + isCorrectClassification(classLabel, vector)
+            fold_accuracies.append(correct/float(len(testSet))) 
+            del linedump
         else:
-            pass 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  #      rm = ruleMining()
- #       (sup,conf) = rm.parseOptions()
-#        rm.cleanUp()
-    #    rm.callApriori(sup,conf,outFile)
-        
+            cluster = []
+            for vector in trainingSet:
+                cluster.append(createAprioriDataVector(vector))
+            aStartTime = time.time()    
+            outfile = rm.callApriori(cluster, support, confidence, -1)
+            aEndTime = time.time()
+            aprioriTime += aEndTime - aStartTime
+            classifier = rm.generateClassifiers([outfile], classes)
+            
+            #Test classifiers on the test set
+            inf = open("FV4.txt", "r")
+            linedump = inf.readlines()
+            inf.close()
+            print ("Classifying test set using generated rules...")
+            for vector in testSet:
+                classLabel = classify(classifier[0], vector)
+                correct = correct + isCorrectClassification(classLabel, vector)
+            fold_accuracies.append(correct/float(len(testSet)))     
+            
+    endTime = time.time()
+    totalTime = endTime - startTime
+    printResults(fold_accuracies, totalTime, aprioriTime, clusteringTime, clusterNum, distanceMetric, support, confidence)
+    cleanDirectory()        
+               
 if __name__ == "__main__":
     main()
     
